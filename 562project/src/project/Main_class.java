b641693b3596e6f44c286223e059bce4ead0de20
vyom-shah <vyom0123@gmail.com>
2020-12-04 16:44:11 -0500

    Update Main_class.java

--- 562project/src/project/Main_class.java
+++ 562project/src/project/Main_class.java
@@ -1,5 +1,24 @@
 package project;
 
+import java.util.HashMap;
+
 public class Main_class {
 
+	public static HashMap<String, String> datatype = new HashMap<String, String>();
+	private void connect() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Driver connected successfully!");
+			
+		}catch(Exception e)
+		{
+			System.out.println("Driver loading Failed!");
+			e.printStackTrace();
+		}
+	}
+	public static void main(String args[]) {
+		Main_class code=new Main_class();
+		code.connect();
+		data
+	}
 }
