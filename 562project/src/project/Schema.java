95b901dcf20e5d3b7d59d06b7231e32ab1c3df4f
vyom-shah <vyom0123@gmail.com>
2020-12-08 22:14:16 -0500

    Create Schema.java

---
+++ 562project/src/project/Schema.java
@@ -0,0 +1,57 @@
+package project;
+
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.HashMap;
+
+public class Schema {
+	private static final String usr="postgres";
+	private static final String password="1234";
+	private static final String url="jdbc:postgres://localhost:5432/CS562";
+	private static HashMap<String, String> dataType=new HashMap<String, String>();
+
+	/**
+	 * get info schema of the DB
+	 */
+	public static HashMap<String, String> getInformationSchema(){
+		try {
+			Connection con = DriverManager.getConnection(url,usr, password);
+			System.out.println("Connection successful");
+			
+			ResultSet resultSet;
+			boolean more;
+			Statement st=con.createStatement();
+			
+			String query="select data_type, column_name from information_schema.columns where table_name= 'sales'";
+			resultSet=st.executeQuery(query);
+			more=resultSet.next();
+			while(more) {
+				if(resultSet.getString("data_type").contains("character")) {
+					datatype.put(resultSet.getString("column_name"),"String");
+				}else{
+					datatype.put(resultSet.getString("column_name"),"int");
+					
+				}
+				more=resultSet.next();
+			}
+			
+		}catch(SQLException e) {
+			System.out.println("Connection error");
+			e.printStackTrace();
+		}
+		return datatype;
+		
+	}
+	public static void outputConnection(PrintWriter writer) {
+		writer.print("\tpublic void connect(){\n");
+        writer.print("\t\ttry {\n");
+        writer.print("\t\tClass.forName(\"org.postgresql.Driver\");\n");
+        writer.print("\t\tSystem.out.println(\"Success loading Driver!\");\n");
+        writer.print("\t\t} catch(Exception exception) {\n");
+        writer.print("\t\texception.printStackTrace();\n");
+        writer.print("\t\t}\n\t}\n");
+	}
+	
+	
+	
+}
