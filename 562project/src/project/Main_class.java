1323cb788a8ac58cc145797e707fae8e35edf63a
vyom-shah <vyom0123@gmail.com>
2020-12-08 22:40:16 -0500

    Update Main_class.java

--- 562project/src/project/Main_class.java
+++ 562project/src/project/Main_class.java
@@ -1,10 +1,12 @@
 package project;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main_class {
 
-	public static HashMap<String, String> datatype = new HashMap<String, String>();
+	public static HashMap<String, String> dataType = new HashMap<String, String>();
 	private void connect() {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -17,8 +19,26 @@ public class Main_class {
 		}
 	}
 	public static void main(String args[]) {
+		File input;
 		Main_class code=new Main_class();
 		code.connect();
+		dataType=Schema.getInformationSchema();
 		
+		System.out.println("Please enter MF of EMF depending on what type of query you want to run");
+		Scanner in= new Scanner(System.in);
+		String query=in.nextLine();
+		
+		if(query.equals("MF"))
+		{
+			input=new File("/Users/vyom/git/cs562/562project/Inputs/MFQuery1.txt");
+			System.out.println("Genereration Successful");
+		}
+		else if(query.equals("MF")) {
+			
+		}
+		else
+		{
+			System.out.println("Please enter a valid option");
+		}
 	}
 }
