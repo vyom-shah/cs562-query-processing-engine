b773c9241b4157cb17d5e59776eb23eee2968306
vyom-shah <vyom0123@gmail.com>
2020-12-09 20:42:37 -0500

    Update MFCode.java

--- 562project/src/project/MFCode.java
+++ 562project/src/project/MFCode.java
@@ -1,5 +1,29 @@
 package project;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
 public class MFCode {
+	
+	public static void codeMF(HashMap<String,String> dataType) {
+		try {
+			File output=new File("/Users/vyom/eclipse-workspace/final_project/src/final_project/MFOp.java");
+			PrintWriter writer=new PrintWriter(output);
+			
+			writer.print("package project;\n");
+			writer.print("import java.sql.*;\n");
+			writer.print("import java.util.*;\n");
+			writer.print("import java.io.*;\n");
+			writer.print("\n");
+			
+			writer.close();
+			
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
